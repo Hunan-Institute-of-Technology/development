18ec6a0ea3d9a5108252f96f613a0d5c0e510e14
superme1one <2459380227@qq.com>
2020-05-15 22:38:17 +0800

    注册页面初次编写

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import Login from "../views/Login.vue";
+import SginUp from "../views/SginUp.vue";
+
 
 Vue.use(VueRouter);
 
@@ -15,6 +17,11 @@ const routes = [
     path: "/login",
     name: "Login",
     component: Login
+  },
+  {
+    path: "/sginup",
+    name: "SginUp",
+    component: SginUp
   }
 ];
 
