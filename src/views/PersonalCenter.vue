956187ced6e0c3747d4b0849dd7c5886aa046056
superme1one <2459380227@qq.com>
2020-05-20 02:02:43 +0800

    个人中心初开发

--- src/views/PersonalCenter.vue
+++ src/views/PersonalCenter.vue
@@ -1,11 +1,151 @@
 <template>
-  <div class="">
-    PersonalCenter
+  <div class="personal">
+    <div class="container">
+      <div class="home-header"></div>
+      <div class="personal-body">
+        <div class="fl per-left">
+          <div class="wrapper">
+            <div class="per-log">
+              <img src="imgs/hearimg.jpg" alt />
+              <p class="per-name">名字</p>
+              <p class="per-college">大学</p>
+              <p class="free-num">闲置数量:15</p>
+            </div>
+            <div class="per-menu">
+              <ul>
+                <li>我的消息</li>
+                <li>关注列表</li>
+                <li @click="checkmenu = 'setupper'">个人设置</li>
+                <li @click="checkmenu = 'release'">发布商品</li>
+                <li @click="checkmenu = 'unused'">我的闲置</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="fr per-right" >
+          <div class="improve-message" v-show="checkmenu == 'setupper'">
+            <p class="title">完善个人信息</p>
+            <table style="margin: auto">
+              <tr>
+                <th>昵称:</th>
+                <th><input type="text"></th>
+              </tr>
+              <tr>
+                <th>昵称:</th>
+                <th><input type="text"></th>
+              </tr>
+               <tr>
+                <th>昵称:</th>
+                <th><input type="text"></th>
+                <th>验证</th>
+              </tr>
+               <tr>
+                <th>昵称:</th>
+                <th><input type="text"></th>
+              </tr>
+            </table>
+            <div class="btn btn-preserve">
+              保存修改信息
+            </div>
+          </div>
+          <div class="release-product" v-show="checkmenu == 'release'">
+            发布商品
+          </div>
+          <div class="my-unused" v-show="checkmenu == 'unused'">
+            我的闲置
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "personal-center",
+  data(){
+    return {
+      checkmenu:"",
+    }
+  }
 };
 </script>
+<style lang="scss" >
+.personal {
+  .home-header {
+    height: 100px;
+    width: 100%;
+    background: burlywood;
+  }
+  .personal-body {
+    .per-left {
+      padding-top: 15px;
+      width: 200px;
+      height: 800px;
+      background: blanchedalmond;
+      .wrapper {
+        text-align: center;
+        .per-log {
+          margin-bottom: 60px;
+          p{
+            margin-bottom:8px ;
+          }
+          img {
+            border-radius: 50%;
+            width: 100px;
+            height: 100px;
+          }
+          .per-name {
+            font-size: 24px;
+            color: blueviolet;
+          }
+          .free-num {
+            font-size: 18px;
+            color: aqua;
+          }
+        }
+        .per-menu{
+          li{
+            font-size: 16px;
+            border-top: 2px solid #e5e5e5;
+            height: 60px;
+            line-height: 60px;
+            &:last-child{
+              border-bottom: 2px solid #e5e5e5;
+            }
+          }
+          
+        }
+      }
+    }
+    .per-right{
+      padding-top: 15px;
+      width: 1026px;
+      background: yellowgreen;
+      height: 800px;
+      .improve-message{
+        text-align: center;
+        .title{
+          font-size: 30px;
+          padding-bottom: 50px;
+        }
+        tr{
+          border: 2px #e5e5e5 solid;
+          height: 80px;
+          line-height: 80px;
+        }
+        .table-border{
+          
+          width: 50px;
+          height: 4px;
+          background: #e5e5e5;
+        }
+        .btn{
+          margin: 0 auto;
+          margin-top: 40px;
+        }
+      }
+    }
+  }
+}
+</style>
