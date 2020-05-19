3a3c4235927667a4073acf6c69f9c6f6be0a5112
superme1one <2459380227@qq.com>
2020-05-19 21:14:23 +0800

    创建个人中心

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Home from "../views/Home.vue";
 import Login from "../views/Login.vue";
 import Index from "../views/Index.vue";
 import SginUp from "../views/SginUp.vue";
+import PersonalCenter from "../views/PersonalCenter.vue";
 
 Vue.use(VueRouter);
 
@@ -31,6 +32,11 @@ const routes = [
     path: "/sginup",
     name: "SginUp",
     component: SginUp
+  },
+  {
+    path:"/personalcenter",
+    name:"PersonalCenter",
+    component:PersonalCenter
   }
 ];
 
---
+++ src/views/PersonalCenter.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="">
+    PersonalCenter
+  </div>
+</template>
+
+<script>
+export default {
+  name: "personal-center",
+};
+</script>
